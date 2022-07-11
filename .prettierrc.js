557c2eddfb4be564a58542c8f6f9bc122dd22fae
Manh Vu <manhdv2103@gmail.com>
2022-07-11 12:04:12 +0700

    update app config

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,26 +1,14 @@
+// very important
 module.exports = {
-  // very important
   arrowParens: 'avoid',
   bracketSameLine: true,
   bracketSpacing: false,
   singleQuote: true,
   trailingComma: 'all',
-  endOfLine: "auto",
+  endOfLine: 'auto',
   formatOnSave: true,
   jsxBracketSameLine: true,
   printWidth: 100,
   semi: true,
   tabWidth: 2,
-
-  // very important
-  // "arrowParens": "avoid",
-  // "bracketSpacing": false,
-  // "endOfLine": "auto",
-  // "formatOnSave": true,
-  // "jsxBracketSameLine": true,
-  // "printWidth": 100,
-  // "semi": true,
-  // "singleQuote": true,
-  // "tabWidth": 2,
-  // "trailingComma": "all"
 };
