1c99e129ed06e3d176a445135bb69d9741302655
Manh Vu <manhdv2103@gmail.com>
2022-07-11 11:28:51 +0700

    rename app to React Native Base

--- android/app/src/main/java/com/reactnativebase/MainActivity.java
+++ android/app/src/main/java/com/reactnativebase/MainActivity.java
@@ -1,4 +1,4 @@
-package com.reduxbase;
+package com.reactnativebase;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
@@ -12,7 +12,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "reduxBase";
+    return "ReactNativeBase";
   }
 
   /**
--- android/app/src/main/java/com/reactnativebase/newarchitecture/components/MainComponentsRegistry.java
+++ android/app/src/main/java/com/reactnativebase/newarchitecture/components/MainComponentsRegistry.java
@@ -1,4 +1,4 @@
-package com.reduxbase.newarchitecture.components;
+package com.reactnativebase.newarchitecture.components;
 
 import com.facebook.jni.HybridData;
 import com.facebook.proguard.annotations.DoNotStrip;
